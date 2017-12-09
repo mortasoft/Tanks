f6703ab3109ba45a8f58f66489366046b0d9c3eb
Mario Zamora <mortasoft@gmail.com>
2017-12-08 21:44:06 -0600

    Format Code

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ app.config['tanks'] = []
 app.config['number'] = 0
 
 # Variables del juego
-width = 336 # Ancho del tablero
-height = 324 # Largo del tablero
-pixWidth = 28 # Ancho del pix
-pixHeight = 27 # Largo del Pix
+width = 336  # Ancho del tablero
+height = 324  # Largo del tablero
+pixWidth = 28  # Ancho del pix
+pixHeight = 27  # Largo del Pix
 
 disparos = []
 
+
 # Tank Class
 class Tank:
-    def __init__(self,x,y,name):
+    def __init__(self, x, y, name):
         self.x = x
         self.y = y
         self.name = name
@@ -26,16 +27,19 @@ class Tank:
 
     # Prints the tank information
     def print(self):
-        return {'x':self.x,'y':self.y,'name':self.name,'direction': self.direction,'hp': self.hp,'alive':self.alive}
+        return {'x': self.x, 'y': self.y, 'name': self.name, 'direction': self.direction, 'hp': self.hp,
+                'alive': self.alive}
+
 
 @app.route("/createTank")
 def createNewTank():
     randomX = randint(27, 300)
     randomY = randint(27, 300)
     number = app.config['number'] = app.config['number'] + 1
-    tank = Tank(randomX,randomY,'Tank_'+str(number))
+    tank = Tank(randomX, randomY, 'Tank_' + str(number))
     app.config['tanks'].append(tank)
     return jsonify(tank.print())
+
 
 @app.route('/shot', methods=['GET'])
 def shot():
@@ -46,8 +50,9 @@ def shot():
             i.hp -= int(hp)
             if i.hp <= 0:
                 i.alive = False
-        return jsonify(hp=i.hp,alive=i.alive,name=i.name)
-    return jsonify({'Error':'Not Found or dead'})
+        return jsonify(hp=i.hp, alive=i.alive, name=i.name)
+    return jsonify({'Error': 'Not Found or dead'})
+
 
 @app.route("/tanks")
 def tanks():
@@ -56,26 +61,28 @@ def tanks():
         out.append(i.print())
     return jsonify(out)
 
+
 @app.route("/tank", methods=['GET'])
 def atank():
-	
-	out = []
-	req = request.args.get('id')
-	found = False
-	for i in app.config['tanks']:
-		if i.name == req:
-			out.append(i.print())
-			found = True
+    out = []
+    req = request.args.get('id')
+    found = False
+    for i in app.config['tanks']:
+        if i.name == req:
+            out.append(i.print())
+            found = True
 
-	if found:
-		return jsonify(out)
-	else:
-		return jsonify({'Error':'Not Found'})
+    if found:
+        return jsonify(out)
+    else:
+        return jsonify({'Error': 'Not Found'})
+
 
 # Direccion principal donde se juega
 @app.route('/Battle-Tanks')
 def game():
     return render_template('CanvasRec.html')
+
 
 # Direccion principal donde se juega
 @app.route('/')
@@ -87,38 +94,43 @@ def root():
 @app.route('/jugada', methods=['GET'])
 def jugar_solo():
     if request.method == 'GET':
-        jugador= request.args.get('jugador')
-        if jugador in ("1","2"):
+        jugador = request.args.get('jugador')
+        if jugador in ("1", "2"):
             num_aleatorio = randint(0, 3)
             if (num_aleatorio == 0):
                 resultado = mover(str(jugador), "izquierda")
-                return jsonify(tanque=jugador, aleatorio=num_aleatorio, accion="movimiento", direccion="izquierda", x=resultado[0],y=resultado[1])
+                return jsonify(tanque=jugador, aleatorio=num_aleatorio, accion="movimiento", direccion="izquierda",
+                               x=resultado[0], y=resultado[1])
             if (num_aleatorio == 1):
                 resultado = mover(str(jugador), "derecha")
-                return jsonify(tanque=jugador, aleatorio=num_aleatorio, accion="movimiento", direccion="derecha", x=resultado[0],y=resultado[1])
+                return jsonify(tanque=jugador, aleatorio=num_aleatorio, accion="movimiento", direccion="derecha",
+                               x=resultado[0], y=resultado[1])
             if (num_aleatorio == 2):
                 resultado = mover(str(jugador), "arriba")
-                return jsonify(tanque=jugador, aleatorio=num_aleatorio, accion="movimiento", direccion="arriba",x=resultado[0],y=resultado[1])
+                return jsonify(tanque=jugador, aleatorio=num_aleatorio, accion="movimiento", direccion="arriba",
+                               x=resultado[0], y=resultado[1])
             if (num_aleatorio == 3):
                 resultado = mover(str(jugador), "abajo")
-                return jsonify(tanque=jugador, aleatorio=num_aleatorio, accion="movimiento", direccion="abajo", x=resultado[0],y=resultado[1])
+                return jsonify(tanque=jugador, aleatorio=num_aleatorio, accion="movimiento", direccion="abajo",
+                               x=resultado[0], y=resultado[1])
             if (num_aleatorio == 4):
-                #mover(jugador, "abajo")
+                # mover(jugador, "abajo")
                 print("Disparo bala 1")
                 return jsonify(tanque=jugador, aleatorio=num_aleatorio, accion="disparar", dato="bala1")
             if (num_aleatorio == 5):
-                #mover(jugador, "abajo")
+                # mover(jugador, "abajo")
                 print("Disparo bala 2")
                 return jsonify(tanque=jugador, aleatorio=num_aleatorio, accion="disparar", dato="bala2")
             if (num_aleatorio == 6):
-                #mover(jugador, "abajo")
+                # mover(jugador, "abajo")
                 print("Disparo bala 3")
                 return jsonify(tanque=jugador, aleatorio=num_aleatorio, accion="disparar", dato="bala3")
         else:
             return jsonify(error="Error, parametros incorrectos")
 
+
 # Metodo que mueve un tanque en una direccion dada
-def mover(tanque,direccion):
+def mover(tanque, direccion):
     global tank2_x
     global tank2_y
     global tank2_direccion
@@ -126,11 +138,11 @@ def mover(tanque,direccion):
     global tank1_y
     global tank1_direccion
 
-    if(tanque=="1"):
-        if direccion=="derecha" :
-            if(tank1_x <=308):
-                tank1_x+=4
-                tank1_direccion="derecha"
+    if (tanque == "1"):
+        if direccion == "derecha":
+            if (tank1_x <= 308):
+                tank1_x += 4
+                tank1_direccion = "derecha"
                 print("Tanque 1 se movio a la derecha")
             else:
                 print("Ya no se puede mover en esa direccion")
@@ -157,7 +169,7 @@ def mover(tanque,direccion):
                 print("Ya no se puede mover en esa direccion")
 
         print("Nueva posicion tanque 1: x:" + str(tank1_x) + " y: " + str(tank1_y))
-        return [tank1_x,tank1_y]
+        return [tank1_x, tank1_y]
 
     if (tanque == "2"):
 
@@ -193,6 +205,7 @@ def mover(tanque,direccion):
         print("Nueva posicion tanque 2: x:" + str(tank2_x) + " y: " + str(tank2_y))
         return tank2_x, tank2_y
 
+
 if __name__ == '__main__':
     print("----Iniciando servidor de Battle Tanks----")
-    app.run('0.0.0.0',8888)
+    app.run('0.0.0.0', 8888)
